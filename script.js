e547f335140f54c210e9c362da50c1e3584158f7
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2024-06-19 22:34:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,16 @@ updateTime();
 setInterval(function() {
   updateTime()
 }, 1000);
+
+
+Date.prototype.yyyymmdd = function() {         
+                                
+        var yyyy = this.getFullYear().toString();                                    
+        var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based         
+        var dd  = this.getDate().toString();             
+                            
+        return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+   };  
+
+d = new Date();
+$('#today').html(d.yyyymmdd());
